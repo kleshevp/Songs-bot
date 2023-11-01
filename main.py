aac6b2c61936690cde4dd7cfcfb42798f3af0911
Pavel <127607439+kleshevp@users.noreply.github.com>
2023-11-01 17:29:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,80 +2,59 @@ import discord
 import requests
 import time
 from discord.ext import commands
+import genius
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.typing = False
 intents.presences = False
-is_playing = {}
+global is_started
+is_started = False
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+@bot.command()
+async def test(ctx):
+    await ctx.send('Бот работает и готов к использованию!')
 
-@bot.event
-async def on_message(message):
-    # Если сообщение впервые доходит до бота, значит песня не играет
-    if message.channel.id not in is_playing:
-        is_playing[message.channel.id] = False
-    # Проверка, чтобы бот не реагировал на свои собственные сообщения
-    if message.author == bot.user:
-        return
-    # Обработка команды "/stop"
-    if message.content.startswith('/stop'):
-        role_id = 1121683287467896955
-        author_roles = [role.id for role in message.author.roles]
-        if role_id in author_roles:
-            if message.channel.id in is_playing:
-                if is_playing[message.channel.id]:
-                    is_playing[message.channel.id] = False
-                    message.channel.send('Песня остановлена')
-                else:
-                    message.channel.send('Песня не играет')
-            else:
-                await message.channel.send("Песня не играет")
-        else:
-            pass
+@bot.command()
+async def song(ctx, msg):
+    global is_started
+    if not is_started:
+        if "-" in msg:
+            author, title = msg.split("-", 1)
+            # Определяем параметры для запроса к API Musixmatch
+            api_key = 'musixmatch API'  # Вставьте ваш ключ API Musixmatch
+            base_url = 'http://api.musixmatch.com/ws/1.1/'
+            method = 'matcher.lyrics.get'
+            params = {
+                'apikey': api_key,
+                'q_artist': author,
+                'q_track': title,
+            }
 
+            response = requests.get(base_url + method, params=params)
             
-    # Обработка команды "/song"
-    elif message.content.startswith('/song'):
-        # Разделение аргументов команды
-        command_args = message.content.split(' ')
-        if len(command_args) < 3:
-            await message.channel.send('Неверная команда. Пример: /song автор песни название песни')
-            return
-
-        author = command_args[1]
-        title = ' '.join(command_args[2:])
-
-        # Определяем параметры для запроса к API Musixmatch
-        api_key = 'musixmatch token'  # Вставьте ваш ключ API Musixmatch
-        base_url = 'http://api.musixmatch.com/ws/1.1/'
-        method = 'matcher.lyrics.get'
-        params = {
-            'apikey': api_key,
-            'q_artist': author,
-            'q_track': title,
-        }
-
-        response = requests.get(base_url + method, params=params)
-
-        if response.status_code == 200:
-            data = response.json()
-            lyrics = data['message']['body']['lyrics']['lyrics_body']
-
-            # Разделение строк текста песни
-            lines = lyrics.split('\n')
-            is_playing[message.channel.id] = True
-
-            # Отправка каждой строки с задержкой в 4 секунды
-            for line in lines:
-                if is_playing[message.channel.id]:
-                    await message.channel.send(line)
-                    time.sleep(4)
-                else:
-                    break
+            if response.status_code == 200:
+                data = response.json()
+                lyrics = data['message']['body']['lyrics']['lyrics_body']
+                await ctx.send(f'{author} {title} заказал {ctx.author.mention}')
+                # Разделение строк текста песни
+                lines = lyrics.split('\n')
+                # Отправка каждой непустой строки с задержкой в 4 секунды
+                for line in lines:
+                    line = line.strip()  # Удаляем начальные и конечные пробелы
+                    if line != "":  # Проверяем, что строка не является пустой
+                        if is_started:
+                            await ctx.send(line)
+                            time.sleep(4)
+                        else:
+                            break
+                    else:
+                        pass
+                is_started = False
+            else:
+                await ctx.send('Песня не найдена.')
         else:
-            await message.channel.send('Песня не найдена.')
+            await ctx.send("Неправильный формат! Используйте: автор-название_песни")
 
-    await bot.process_commands(message)
-
-bot.run('Discord token')
+bot.run('Discord API')
